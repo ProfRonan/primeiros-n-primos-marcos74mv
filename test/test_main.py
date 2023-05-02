aaada4378c5c2739d158124515555e285f45a1e7
Ronan Pardo Soares <ronan.soares@gmail.com>
2023-05-02 09:17:35 -0300

    Update tests adding docker and reorganizing files

--- test/test_main.py
+++ test/test_main.py
@@ -1,10 +1,11 @@
 """Test file for testing the main.py file"""
 
-import unittest
-from unittest.mock import patch
+import unittest # for creating the test case
+from unittest.mock import patch # for mocking the input
 import io # for capturing the output
 import sys # for restoring the stdout and removing the main module from the cache
 import importlib # for importing the main.py file
+from pathlib import Path # for getting the path of the main.py file
 
 class TestMain(unittest.TestCase):
     """Class for testing the main.py file"""
@@ -21,7 +22,7 @@ class TestMain(unittest.TestCase):
         sys.stdout = captured_output
         importlib.import_module("main")
         sys.stdout = sys.__stdout__
-        self.assertIn('', captured_output.getvalue().strip())
+        self.assertIn("2", captured_output.getvalue().strip())
 
     @patch("builtins.input", return_value="5")
     def test_5(self, _mock_input):
@@ -30,7 +31,7 @@ class TestMain(unittest.TestCase):
         sys.stdout = captured_output
         importlib.import_module("main")
         sys.stdout = sys.__stdout__
-        self.assertIn('', captured_output.getvalue().strip())
+        self.assertIn("2\n3\n5\n7\n11", captured_output.getvalue().strip())
 
     @patch("builtins.input", return_value="20")
     def test_20(self, _mock_input):
@@ -39,8 +40,11 @@ class TestMain(unittest.TestCase):
         sys.stdout = captured_output
         importlib.import_module("main")
         sys.stdout = sys.__stdout__
-        self.assertIn('', captured_output.getvalue().strip())
-
+        self.assertIn("2\n3\n5\n7\n11\n13\n17\n19\n23\n29\n31\n37"
+                      "\n41\n43\n47\n53\n59\n61\n67\n71", captured_output.getvalue().strip())
 
 if __name__ == "__main__":
+    # add the parent directory to the path in order to run it from the run command in vscode
+    main_file_folder = Path(__file__).parents[1].as_posix() # pylint: disable=invalid-name
+    sys.path.insert(0, main_file_folder)
     unittest.main()
